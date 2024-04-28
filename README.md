9ed346a3ddc4c0e8574440c231d9cf3a5e3a8d4b
Redy1908 <58226371+Redy1908@users.noreply.github.com>
2024-04-28 17:02:09 +0200

    Updated

--- README.md
+++ README.md
@@ -2,6 +2,7 @@
 
 ## Software i'm using
 - [Alacritty](https://github.com/alacritty/alacritty)
+- [Tmux](https://github.com/tmux/tmux/wiki)
 - [Starship](https://github.com/starship/starship)
 - [Zoxide](https://github.com/ajeetdsouza/zoxide)
 - [Lsd](https://github.com/lsd-rs/lsd)
---
+++ install.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+# Neovim
+sudo add-apt-repository ppa:neovim-ppa/unstable -y
+
+# Update and Upgrade
+sudo apt update
+sudo apt upgrade
+
+# Some utility packages
+sudo apt install make gcc ripgrep unzip git xclip neovim stow tmux
+
+# Starship
+curl -sS https://starship.rs/install.sh | sh
+
+if ! grep -Fxq 'eval "$(starship init bash)"' ~/.bashrc
+then
+    echo 'eval "$(starship init bash)"' >> ~/.bashrc
+fi
+
+# Zoxide
+sudo apt install zoxide
+if ! grep -Fxq 'eval "$(zoxide init bash)"' ~/.bashrc
+then
+    echo 'eval "$(zoxide init bash)"' >> ~/.bashrc
+fi
+
+# Rust
+curl https://sh.rustup.rs -sSf | sh
+
+# LSD
+cargo install lsd
+
+# Bat
+sudo apt install bat
+mkdir -p ~/.local/bin
+ln -s /usr/bin/batcat ~/.local/bin/bat
+
+#  ---
+
+if ! grep -Fxq "alias dev=~/dev.sh" ~/.bashrc
+then
+    echo "alias dev=~/dev.sh" >> ~/.bashrc
+fi
+
+stow .
+
+chmod +x ~/dev.sh 
+
+source ~/.bashrc
