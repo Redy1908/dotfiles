81d86838acedff1800827e8b48e05abab9019f60
Redy1908 <58226371+Redy1908@users.noreply.github.com>
2024-10-04 14:33:54 +0200

    update

--- cDeveloping.sh
+++ cDeveloping.sh
@@ -13,7 +13,7 @@ fi
 mkdir -p "$1"
 cd "$1" || exit
 
-mkdir -p src obj bin
+mkdir -p src obj bin results 
 
 cat <<EOL > src/main.c
 #include <stdio.h>
@@ -25,5 +25,7 @@ int main() {
 EOL
 
 printf "CC=gcc\nCFLAGS=-I. -fopenmp\n\nall: bin/main\n\nbin/main: obj/main.o\n\t\$(CC) -o bin/main obj/main.o \$(CFLAGS)\n\nobj/main.o: src/main.c\n\t\$(CC) -c src/main.c -o obj/main.o \$(CFLAGS)\n\nclean:\n\trm -f obj/*.o bin/main\n" > Makefile
+
+printf "#!/bin/bash\n\n#PBS -N myJob\n#PBS -q studenti\n#PBS -o results/result.out\n#PBS -e results/result.err\n\ncd \$PBS_O_WORKDIR\n\nif [ -f results/result.out ]; then\n    rm results/result.out\nfi\n\nif [ -f results/result.err ]; then\n    rm reusults/result.err\nfi\n\necho 'Working directory: ' \$(pwd)\n\nmake\n\n./bin/main" > $1.pbs
 
 echo "Started C developing."
--- dev.sh
+++ dev.sh
@@ -11,7 +11,8 @@ tmux new-session -d -s dev -c $DIR
 
 tmux new-window -t dev:1 -n 'dev'    -c $DIR
 tmux new-window -t dev:2 -n 'run'    -c $DIR 
-tmux new-window -t dev:3 -n 'docker' -c $DIR
+tmux new-window -t dev:3 -n 'ssh'    -c $DIR 
+tmux new-window -t dev:4 -n 'sftp'   -c $DIR 
+tmux new-window -t dev:5 -n 'docker' -c $DIR
 
 tmux -2 attach-session -d -t dev
-
