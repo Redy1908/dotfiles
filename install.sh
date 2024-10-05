9094fba25cad285f168e3d18981104002cf177e3
Redy1908 <58226371+Redy1908@users.noreply.github.com>
2024-10-05 20:58:02 +0200

    update

--- install.sh
+++ install.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
 # Update and Upgrade
+
 sudo apt update
 sudo apt upgrade
+
 
 # Some utility packages
 sudo apt install make gcc ripgrep unzip git xclip neovim stow tmux zoxide bat lsd
 
+
 # Neovim
+
 sudo add-apt-repository ppa:neovim-ppa/unstable -y
 
 # Starship
@@ -15,7 +19,7 @@ curl -sS https://starship.rs/install.sh | sh
 
 if ! grep -Fxq 'eval "$(starship init bash)"' ~/.bashrc
 then
-    echo 'eval "$(starship init bash)"' >> ~/.bashrc
+    echo -e '\neval "$(starship init bash)"' >> ~/.bashrc
 fi
 
 # Zoxide
@@ -24,6 +28,7 @@ then
     echo 'eval "$(zoxide init bash)"' >> ~/.bashrc
 fi
 
+
 # Bat
 mkdir -p ~/.local/bin
 ln -s /usr/bin/batcat ~/.local/bin/bat
@@ -31,10 +36,30 @@ ln -s /usr/bin/batcat ~/.local/bin/bat
 # Move the files to the correct locations
 stow .
 
+# Aliases for the commands
+
+if ! grep -Fxq 'alias cd='z'' ~/.bashrc
+then
+
+	echo -e "\nalias cd='z'" >> ~/.bashrc
+fi
+
+if ! grep -Fxq 'alias cat='bat'' ~/.bashrc
+then
+	echo "alias cat='bat'" >> ~/.bashrc
+fi
+
+if ! grep -Fxq 'alias ls='lsd'' ~/.bashrc
+
+then
+	echo "alias ls='lsd'" >> ~/.bashrc
+fi
+
 # Aliases for the scripts
 if ! grep -Fxq "alias dev=~/dev.sh" ~/.bashrc
 then
-    echo "alias dev=~/dev.sh" >> ~/.bashrc
+
+    echo -e "\nalias dev=~/dev.sh" >> ~/.bashrc
 fi
 
 if ! grep -Fxq "alias cdeveloping=~/cDeveloping.sh" ~/.bashrc
@@ -45,5 +70,6 @@ fi
 # Run permission for the scripts
 chmod +x ~/dev.sh
 chmod +x ~/cDeveloping.sh
+
 
 source ~/.bashrc
